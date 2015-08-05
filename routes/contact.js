c1f077919b5efe4e80150b2baac7727ac4194962
chadcates <cates.chad@gmail.com>
2015-08-05 15:34:12 -0500

    made changes to contact.js file for nodemailer

--- routes/contact.js
+++ routes/contact.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+	service: 'gmail',
+    auth: {
+        user: 'cates.chad@gmail.com',
+        pass: ''
+    }
+});
+
+
+router.post('/', function (req, res){
+
+})
 
 router.get('/', function (req, res) {
     res.render('templates/contact');
-	var nodemailer = require('nodemailer');
-    var transporter = nodemailer.createTransport();
-    transporter.sendMail({from: 'cates.chad@gmail', to: 'cates.chad@gmail.com', subject: 'James is a smart guy', text: 'I love big real tities'});
-    console.log('kittens');
+    transporter.sendMail({
+    from: 'Lenny',
+    to: 'cates.chad@gmail.com',
+    subject: 'hello',
+    text: 'Hey Chad! Looking good today. Sailor.'
+});
+    console.log('Captain Chad!')
 });
 
 module.exports = router;
